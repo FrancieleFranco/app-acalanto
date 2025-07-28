52c60a183e8b91e511cc482561cb504d362ac60a
Franciele Franco <fran.alves1108@gmail.com>
2025-07-28 20:49:10 -0300

    create component buttom

--- src/app/features/home/pages/home/home.module.ts
+++ src/app/features/home/pages/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { ButtonComponent } from 'src/app/shared/components/button/button.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+
   ],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent,ButtonComponent],
   exports: [HomeComponent]
 })
 export class HomeModule { }
