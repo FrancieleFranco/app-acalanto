1f30acc278251a0d97f9f8436c4a07f6f11e67ad
Franciele Franco <fran.alves1108@gmail.com>
2025-07-29 14:29:18 -0300

    ts file adjustments

--- src/app/features/home/pages/home/home.component.ts
+++ src/app/features/home/pages/home/home.component.ts
@@ -5,7 +5,7 @@ import { MessageService } from 'src/app/core/service/message.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
